38d27494cb2108d9b20429d16a055f11c7740a5b
onkar-singh <omkarsinghml@gmail.com>
2023-03-23 11:00:46 +0530

    bn

---
+++ keyboardShortcuts.js
@@ -0,0 +1,48 @@
+/*****************************************************************
+    option+click    -   insert cursor
+shift+option+click  -   colmun selection
+    cmd+up/down     -   go end/begning of document
+shift+up/down/left/right    -   select text
+    option+F        -   copy file path
+    cmd+C/X         -   copy/cut line
+-----------------------------------------------------------------
+myy.js
+
+
+my.js
+/// sync
+
+om.js
+
+
+del od from party details
+
+-----------------------------------------------------------------
+out order list optimise
+ remove inline css class
+
+
+myy.js
+281 save price in order obj // my.js 304
+
+add 
+update-dont update price
+del
+
+tot() - if old odprice available in order obj then oldprice(save in localstorage)
+
+
+send unpin and update order data combine
+send pin and new order data combine
+
+share api Navigator.share() share text,image
+
+copy to clipboard
+
+convert blob to file to share
+
+
+
+-----------------------------------------------------------------
+*****************************************************************/
+
